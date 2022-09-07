81ba53fb95703a870555aa55a1d3efa8d2b1e2c7
Amine-92-03 <mefti.amine92@gmail.com>
2022-09-07 17:00:50 +0200

    ...

--- Controllers/saveDataToDb.js
+++ Controllers/saveDataToDb.js
@@ -1,15 +1,18 @@
 import mongoDb from 'mongodb'
 import sendmail from './sendMail.js'
 var MongoClient = mongoDb.MongoClient;
+import dotenv from 'dotenv' 
+dotenv.config()
+
 var Server = mongoDb.Server
 // console.log(Server);
 let uri = 'mongodb://localhost:27017/'
 var dbName = 'DonnéesParkingsTempsReel'
 
+let url_cosmosDb =  process.env.COSMOS_CONNECTION_STRING;
 
 
-
-const client = new MongoClient(uri);
+const client = new MongoClient(url_cosmosDb);
 (async () => await client.connect())();
 
 // use client to work with db
